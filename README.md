f08db7d4e89ff246142babed1edb2b6ac57f3f5c
Andrey Stoychev <avs44@cam.ac.uk>
2020-10-13 14:38:36 +0100

    Prepare code for splitting into branches
    Replaced group-specific information (people, slots, constraints) with
    made-up information.
    
    Added an option to print schedules as tables, for easy copy-pasting to
    spreadsheets.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Overview
-A little programme for solving scheduling problems using a constraint programming (CP) solver [OR-Tools](https://github.com/google/or-tools).
+A little programme for solving scheduling problems using the constraint programming (CP) solver in [OR-Tools](https://github.com/google/or-tools).
 
-It defines a list of people and meeting slots, adds Boolean variables to the CP model for every combination of people and slots and for every combination of 4 people per slot, and adds logical and linear constraints on the variables to the CP model. Then, it solves the model and prints out solutions and statistics.
+It defines a list of people and meeting slots, adds Boolean variables to the CP model for every combination of people and slots and for every combination of N people per slot (can be changed within the code), and adds logical and linear constraints on the variables to the CP model. Then, it solves the model and prints out solutions and statistics.
 
 ## Usage
 First, make sure you have Python 3. Then, [install OR-Tools](https://developers.google.com/optimization/install).
@@ -25,30 +25,8 @@ There are typically too many solutions to wait for the programme to finish by it
 
 The CP solver seems to be deterministic, as it always produces the same solutions in the same order, given the same set of variables and constraints. 
 
-By default, only the first solution is printed to standard output. If you want to print more solutions, change `solutions_to_print` to another integer. And if you want to print specific solutions, change `solutions_to_print` to a list of solutions numbers. E.g. `solutions_to_print = [0 4 9]` will print the first, fifth, and tenth solutions.
-
 ## Constraints
 
 ### Global
 
-Each meeting has exactly 4 people.
-<br>
-Each person has at most one meeting per day.
-<br>
-Each person has at least three meetings per week.
-<br>
-Each person has at most four meetings per week.
-<br>
-All meetings are unique in a week. I.e. every combination of 4 people occurs at most once across all slots in a week.
-
 ### Personal
-
-Andy cannot meet on Tuesdays.
-<br>
-Adriano cannot meet on mornings.
-<br>
-Adriano cannot meet on Fridays.
-<br>
-Saeed cannot meet on afternoons.
-<br>
-Adriana cannot meet on Wednesdays.
--- schedule_meetings.py
+++ schedule_meetings.py
@@ -7,23 +7,22 @@ import ortools.sat.python.cp_model
 
 
 people = [
-	'Dhruva', 'Michael', 'Monika',
-	'Adriano', 'Andy', 'Adriana',
-	'Thomas', 'Ethan', 'Prannoy',
-	'Saeed', 'Weiming'
+	'Alice', 'Bob', 'Carol',
+	'David', 'Eve', 'Frank',
+	'Grace', 'Hal', 'Irene'
 ]
 slots = {
-	'Mon': ['10:00'],
-	'Tue': ['10:00', '14:00'],
-	'Wed': ['10:00', '14:00'],
-	'Thu': ['10:00'],
-	'Fri': ['10:00']
+	'Mon': ['11:00'],
+	'Tue': ['10:00', '11:00'],
+	'Wed': ['10:00'],
+	#'Thu': ['10:00'],
+	'Fri': ['13:00', '14:00', '15:00']
 }
-num_people_per_meeting = 4
+num_people_per_meeting = 5
 
 
 class PartialSolutionPrinter(ortools.sat.python.cp_model.CpSolverSolutionCallback):
-	def __init__(self, meetings, solutions_to_print, max_solutions):
+	def __init__(self, meetings, solutions_to_print, max_solutions, table):
 		ortools.sat.python.cp_model.CpSolverSolutionCallback.__init__(self)
 		self._meetings = meetings
 		if isinstance(solutions_to_print, int):
@@ -34,19 +33,43 @@ class PartialSolutionPrinter(ortools.sat.python.cp_model.CpSolverSolutionCallbac
 			raise Exception
 		self._max_solutions = max_solutions
 		self._solution_count = 0
+		self._table = table
 
 	def on_solution_callback(self):
 		if self._solution_count in self._solutions_to_print:
 			print('== Solution %i ==' % self._solution_count)
-			for day in list(slots.keys()):
-				for time in slots[day]:
-					print('%s, %s:  ' % (day, time), end='')
-					for person in people:
-						if self.Value(self._meetings[(day, time, person)]):
-							print('%s ' % person, end='')
+			if self._table == False:
+				for day in list(slots.keys()):
+					for time in slots[day]:
+						print('%s, %s:  ' % (day, time), end='')
+						for person in people:
+							if self.Value(self._meetings[(day, time, person)]):
+								print('%s ' % person, end='')
+						print()
 					print()
 				print()
-			print()
+			else:
+				max_num_slots = max([len(x) for x in slots.values()])
+				schedule = [[[] for y in range(max_num_slots)] for x in slots.keys()]
+
+				for day_idx, day in enumerate(list(slots.keys())):
+					for time_idx, time in enumerate(slots[day]):
+						schedule[day_idx][time_idx].extend([person for person in people if self.Value(self._meetings[(day, time, person)])])
+
+				num_days = len(slots.keys())
+
+				for time_idx in range(max_num_slots):
+					for person_idx in range(num_people_per_meeting):
+						for day_idx in range(num_days):
+							sep = '\t'
+							if day_idx == num_days - 1:
+								sep = ''
+							person = ' '
+							if len(schedule[day_idx][time_idx]) > 0:
+								person = schedule[day_idx][time_idx][person_idx]
+							print('%s%s' % (person, sep), end='')
+						print()
+					print()
 		self._solution_count += 1
 		if self._max_solutions is not None and self._solution_count >= self._max_solutions:
 			self.StopSearch()
@@ -65,15 +88,15 @@ def main(args):
 			for person in people:
 				meetings[(day, time, person)] = model.NewBoolVar('%s__%s__%s' % (day, time, person))
 
-	# Each meeting has exactly 4 people.
+	# Each meeting has exactly 5 people.
 	for day in list(slots.keys()):
 		for time in slots[day]:
 			model.Add(sum(meetings[(day, time, person)] for person in people) == num_people_per_meeting)
 
 	# Each person has at most one meeting per day.
-	for person in people:
-		for day in list(slots.keys()):
-			model.Add(sum(meetings[(day, time, person)] for time in slots[day]) <= 1)
+	#for person in people:
+	#	for day in list(slots.keys()):
+	#		model.Add(sum(meetings[(day, time, person)] for time in slots[day]) <= 1)
 
 	# Each person has at least two meetings per week.
 	for person in people:
@@ -92,39 +115,30 @@ def main(args):
 				model.AddMinEquality(c[-1], [meetings[(day, time, person)] for person in combination])
 		model.Add(sum(c) <= 1)
 
-	# Andy cannot meet on Tuesdays.
-	model.Add(sum(meetings[('Tue', time, 'Andy')] for time in slots['Tue']) == 0)
+	# Alice cannot meet on Tuesdays.
+	#model.Add(sum(meetings[('Tue', time, 'Alice')] for time in slots['Tue']) == 0)
 
-	# Adriano cannot meet on mornings.
-	model.Add(sum(meetings[(day, time, 'Adriano')] for day in list(slots.keys()) for time in slots[day] if datetime.datetime.strptime(time, '%H:%M') < datetime.datetime.strptime('14:00', '%H:%M')) == 0)
-
-	# Adriano cannot meet on Fridays.
-	model.Add(sum(meetings[('Fri', time, 'Adriano')] for time in slots['Fri']) == 0)
-
-	# Saeed cannot meet on afternoons.
-	model.Add(sum(meetings[(day, time, 'Saeed')] for day in list(slots.keys()) for time in slots[day] if datetime.datetime.strptime(time, '%H:%M') >= datetime.datetime.strptime('14:00', '%H:%M')) == 0)
-
-	# Adriana cannot meet on Wednesdays.
-	model.Add(sum(meetings[('Wed', time, 'Adriana')] for time in slots['Wed']) == 0)
+	# Frank cannot meet on mornings.
+	#model.Add(sum(meetings[(day, time, 'Frank')] for day in list(slots.keys()) for time in slots[day] if datetime.datetime.strptime(time, '%H:%M') < datetime.datetime.strptime('12:00', '%H:%M')) == 0)
 
 	solver = ortools.sat.python.cp_model.CpSolver()
 	solver.parameters.linearization_level = 0
 
 	if args['rand'] is not None:
 		sols = random.sample(range(args['rand'][1]), args['rand'][0])
-		solution_printer = PartialSolutionPrinter(meetings, sols, max(sols) + 1)
+		solution_printer = PartialSolutionPrinter(meetings, sols, max(sols) + 1, args['table'])
 	elif args['num'] is not None:
-		solution_printer = PartialSolutionPrinter(meetings, list(range(args['num'])), None)
+		solution_printer = PartialSolutionPrinter(meetings, list(range(args['num'])), None, args['table'])
 	elif args['id'] is not None:
-		solution_printer = PartialSolutionPrinter(meetings, args['id'], max(args['id']) + 1)
+		solution_printer = PartialSolutionPrinter(meetings, args['id'], max(args['id']) + 1, args['table'])
 	solver.SearchForAllSolutions(model, solution_printer)
 
 	# Statistics.
 	print()
 	print('Statistics')
-	print('  - conflicts	   : %i' % solver.NumConflicts())
-	print('  - branches		: %i' % solver.NumBranches())
-	print('  - wall time	   : %f s' % solver.WallTime())
+	print('  - conflicts       : %i' % solver.NumConflicts())
+	print('  - branches        : %i' % solver.NumBranches())
+	print('  - wall time       : %f s' % solver.WallTime())
 	print('  - solutions found : %i' % solution_printer.solution_count())
 
 
@@ -134,6 +148,7 @@ if __name__ == '__main__':
 	group.add_argument('--rand', action='store', metavar=('N', 'M'), dest='rand', type=int, nargs=2, help='print N random solutions from the first M solutions')
 	group.add_argument('--num', action='store', metavar='N', dest='num', type=int, help='print the first N solutions')
 	group.add_argument('--id', action='store', metavar='ID', dest='id', type=int, nargs='+', help='print the solutions with the specified IDs')
+	parser.add_argument('--table', action='store_true', dest='table', help='instead of the default verbose output, print a tab-separated table that can directly be pasted into a spreadsheet')
 	args = parser.parse_args()
 
 	main(vars(args))
